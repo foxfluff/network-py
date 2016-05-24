7365854c0538f6a85270388201fd5accdc3bad51
foxfluff <foxfluff@users.noreply.github.com>
2016-05-24 12:58:29 -0400

    building skeleton for net attributes, basic structure, etc

--- network/ipv4_network.py
+++ network/ipv4_network.py
@@ -2,7 +2,40 @@
 import network
 
 class Ipv4Net(network.Network):
-    pass
+    
+    def __init__(self, net_id, netmask):
+        self.network_id = net_id
+        pass
+
+    @property
+    def network_id(self):
+        return self._netid
+
+    @network_id.setter
+    def network_id(self, addr):
+        # TODO: calc actual net id based on netmask
+        self._netid = addr
+
+    @property
+    def netmask(self):
+        return self._mask
+
+    @netmask.setter
+    def netmask(self, addr)
+        # TODO: error checking for consecutive bits/proper mask
+        self._mask = addr
+
+    # do i even need these functions or should I instead use something like pop/set/builtins etc
+    def addhost(self, host):
+        # TODO: make sure it's isinstance(ipv4) etc etc
+        pass
+
+    def delhost(self, host):
+        # TODO: everything o_o
+        pass
+
+
+
 
 
 __all__ = ['Ipv4Net']
