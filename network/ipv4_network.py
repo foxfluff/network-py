540a734a08dd749dbf8bc1a78b9873ff59e88158
foxfluff <foxfluff@users.noreply.github.com>
2016-06-14 08:33:35 -0400

    Implemented netmask checking and netid auto-correction to lowest address in the given subnet

--- network/ipv4_network.py
+++ network/ipv4_network.py
@@ -1,10 +1,11 @@
 import network
+import math
 
 class Ipv4Net(network.Network):
     
     def __init__(self, net_id, netmask):
-        self.network_id = net_id
         self.netmask = netmask
+        self.network_id = net_id
 
     @property
     def network_id(self):
@@ -12,17 +13,21 @@ class Ipv4Net(network.Network):
 
     @network_id.setter
     def network_id(self, addr):
-        # TODO: calc actual net id based on netmask
-        self._netid = addr
+        self._netid = addr & self.netmask
 
     @property
     def netmask(self):
         return self._mask
 
     @netmask.setter
-    def netmask(self, addr)
-        # TODO: error checking for consecutive bits/proper mask
-        self._mask = addr
+    def netmask(self, addr):
+        # TODO: Bug test
+        cidr = int(math.log(addr + 1, 2))
+        if 2**cidr != addr + 1:
+            raise ValueError("Improper value for netmask %s, must be address \
+                             with only consecutive bits" %addr)
+        else:
+            self._mask = addr
 
     # do i even need these functions or should I instead use something like pop/set/builtins etc
     def addhost(self, host):
