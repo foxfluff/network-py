a62c7f38ad3e88afc21bf83fa207a75f7c5c85cc
foxfluff <foxfluff@users.noreply.github.com>
2016-06-14 08:47:35 -0400

    Minor tweak with netmask checking that should actually work this time.

--- network/ipv4_network.py
+++ network/ipv4_network.py
@@ -22,8 +22,7 @@ class Ipv4Net(network.Network):
     @netmask.setter
     def netmask(self, addr):
         # TODO: Bug test
-        cidr = int(math.log(addr + 1, 2))
-        if 2**cidr != addr + 1:
+        if math.log(addr + 1, 2) % 1: #Should resolve to 0 (aka False) when a real netmask
             raise ValueError("Improper value for netmask %s, must be address \
                              with only consecutive bits" %addr)
         else:
